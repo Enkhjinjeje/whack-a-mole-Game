dcc3d04ac5cefb693b8b3c1bd3094824cd7829a2
name <email>
2021-08-27 14:03:49 -0700

    finished final score and explosion

--- whack a mole/whack.js
+++ whack a mole/whack.js
@@ -4,6 +4,8 @@ let squares = document.querySelectorAll(".square");
 let scoreBoard = document.querySelector("#score");
 // makeing var called timeboard with id class
 let timeboard = document.querySelector("#time");
+const messageBoard = document.querySelector("#message");
+let finishPoint = document.querySelector("#finishScore");
 // createing lastSquare with let
 let lastSquare = squares;
 // createing timeUp with let and calling it false
@@ -11,18 +13,24 @@ let timeUp = false;
 // creating score with let and value is 0
 let score = 0;
 // creating timeLeft and value 30
-let timeLeft = 30;
+let timeLeft = 10;
+let restartButton = document.querySelector("#restartButton");
+
 // creating function called downloadTimer and seting-setInterval with arrow function and counting 1 sec wich is 1000
-let downloadTimer = setInterval(() => {
-  // if timeleft is same or lower than 0 its clearing-interval
-  if (timeLeft <= 0) {
-    clearInterval(downloadTimer);
-  } else {
-    timeboard.innerHTML = timeLeft;
-  }
-  // else  its connected to time board and uses innerHTML and is -=1 with timeLeft wich is -1 every time the function runs
-  timeLeft -= 1;
-}, 1000);
+//
+function downloadTimer() {
+  const interval = setInterval(() => {
+    // if timeleft is same or lower than 0 its clearing-interval
+    if (timeLeft <= 0) {
+      clearInterval(interval);
+    } else {
+      timeboard.innerHTML = timeLeft;
+    }
+
+    // else  its connected to time board and uses innerHTML and is -=1 with timeLeft wich is -1 every time the function runs
+    timeLeft -= 1;
+  }, 1000);
+}
 // creating function named randomNumber
 function randomNumber() {
   // inside randomNumber its getting random number with math.floor and math.random wich is giveing random number betwenn 0 to 17
@@ -50,18 +58,20 @@ function randomHoles(holes) {
 
   return square;
 }
+restartButton.addEventListener("click", () => {
+  messageBoard.classList.remove("show");
+  startgame();
+});
 // creating a function named startGame
 function startgame() {
   // inside startGame timeUp = false and scoreboard is = 0 score=0 and running peep seting setTimeout wich is runing timeUP with 30000
+  timeLeft = 10;
   timeUp = false;
   scoreBoard.textContent = 0;
   score = 0;
+  downloadTimer();
   peep();
-  setTimeout(() => (timeUp = true), 30000);
-}
-// if timeup = true console.log we finished
-if (timeUp === true) {
-  console.log("we finishid");
+  setTimeout(() => (timeUp = true), 10000);
 }
 // creating function named peep with randomTime first is min and second is max and getting randomholes and adding astro
 function peep() {
@@ -76,7 +86,8 @@ function peep() {
       peep();
       // else we are done
     } else {
-      console.log("we are done");
+      messageBoard.classList.add("show");
+      finishPoint.textContent = score;
     }
   }, time);
 }
@@ -93,6 +104,11 @@ function destroy(event) {
     event.target.classList.remove("astro");
     score++;
     scoreBoard.textContent = score;
+    finishPoint.textContent = score;
+    event.target.classList.add('explosion')
+    setTimeout(() => {
+      event.target.classList.remove('explosion')
+    }, 500);
   }
 }
 squares.forEach((squares) => {
